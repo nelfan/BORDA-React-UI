0e12fb2ed4f046305840e8ed1db65752360b0cdb
ChernykhVyacheslav <chernihvyacheslav@gmail.com>
2021-05-27 14:21:43 +0300

    Implemented Login component that successfully returns jwt token from server upon log in

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,17 @@
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from '../Home/Home';
+import Login from '../Auth/Login/Login';
+import Register from '../Auth/Register/Register';
+import Auth from '../Auth/Auth';
 
 function App() {
   return (
     <Router>
       <Switch>
         <Route path='/' exact={true} component={Home} />
+        <Route path='/auth' exact={true} component={Auth} />
+        <Route path='/login' exact={true} component={Login} />
+        <Route path='/register' exact={true} component={Register} />
       </Switch>
     </Router>
   );
