0e4aedc69dede2bb85d7916480294b67d57dc5ac
ChernykhVyacheslav <chernihvyacheslav@gmail.com>
2021-05-28 10:23:03 +0300

    Fixed the necessity to use timout before redirect

--- src/components/Auth/Login/Login.js
+++ src/components/Auth/Login/Login.js
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import '../auth.css'
 
-const Login = () => {
+const Login = ({onSubmitAuth}) => {
     const [username, setUsername] = useState('')
     const [password, setPassword] = useState('')
 
@@ -40,6 +40,8 @@ const Login = () => {
         const data = await res.json()
 
         sessionStorage.setItem('jwtToken', data.token)
+        
+        onSubmitAuth(data.token)
 
         return data
     }
--- src/components/Auth/Register/Register.js
+++ src/components/Auth/Register/Register.js
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import '../auth.css';
 
-const Register = () => {
+const Register = ({onSubmitAuth}) => {
     const [username, setUsername] = useState('')
     const [firstName, setFirstName] = useState('')
     const [lastName, setLastName] = useState('')
@@ -39,6 +39,8 @@ const Register = () => {
         const data = registerUser(username, firstName, lastName, email, password)
 
         sessionStorage.setItem('jwtToken', data.token)
+
+        onSubmitAuth(data.token)
 
         return data
     }
