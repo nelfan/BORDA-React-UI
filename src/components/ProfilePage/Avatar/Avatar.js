8519270183f9d3d8d4544b4bae57f4e234d559e2
jules <neroba.yulia@gmail.com>
2021-05-31 16:49:12 +0300

    Minor changes

--- src/components/ProfilePage/Avatar/Avatar.js
+++ src/components/ProfilePage/Avatar/Avatar.js
@@ -3,7 +3,7 @@ import React from 'react';
 
 const Avatar = React.forwardRef((props, ref) => {
     return (
-        <img className="avatar"  ref = {ref} onClick={props.onClick} src={props.src} width={props.width} alt="User's avatar" accept=".jpg, .jpeg, .png"></img>
+        <img className="avatar"  ref={ref} onClick={props.onClick} src={props.src} width={props.width} alt="User's avatar" accept=".jpg, .jpeg, .png"></img>
     )
     })
 
