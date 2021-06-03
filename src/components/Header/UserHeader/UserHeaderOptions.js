cb115d763d024b6ed6359cb77f156860af157354
David Chursa <David.Chursa@teoco.com>
2021-06-03 13:22:07 +0300

    move to functional components UserHeaderMenu && UserHeaderOptions && UserNotifications

--- src/components/Header/UserHeader/UserHeaderOptions.js
+++ src/components/Header/UserHeader/UserHeaderOptions.js
@@ -1,48 +1,40 @@
-import React, {Component} from 'react';
+import React, {useState} from 'react';
 import "./user_header_options.css"
 import UserHeaderMenu from "./UserHeaderMenu/UserHeaderMenu";
 import UserNotifications from "./UserNotifications/UserNotifications";
 
-class UserHeaderOptions extends Component {
-    state = {
-        showMenu: false,
-        menuNotification: false
+function UserHeaderOptions() {
+    const [userMenu, setStateUserMenu] = useState(false)
+    const [notificationMenu, setStateNotificationMenu] = useState(false)
+
+    const clickUserMenu = () => {
+        setStateUserMenu(!userMenu);
     }
 
-    clickUserMenu = () => {
-        this.setState({
-            showMenu: !this.state.showMenu
-        })
+    const clickNotificationIcon = () => {
+        setStateNotificationMenu(!notificationMenu);
     }
 
-    clickNotificationIcon = () => {
-        this.setState({
-            menuNotification: !this.state.menuNotification
-        })
-    }
-
-    render() {
-        return <div className="right-side">
-            <div className="alignment_of_items">
-                <div className="empty_block"/>
-                <div className="general-info">
-                    <a href="#" className="user_icon">
-                        <i className="fa fa-user-circle" onClick={this.clickUserMenu}>
-                            {this.state.showMenu ? <UserHeaderMenu/> : null}
+    return <div className="right-side">
+        <div className="alignment_of_items">
+            <div className="empty_block"/>
+            <div className="general-info">
+                <a href="#" className="user_icon">
+                    <i className="fa fa-user-circle" onClick={clickUserMenu}>
+                        {userMenu ? <UserHeaderMenu/> : null}
+                    </i>
+                </a>
+                <a className="notification_icon">
+                    <div className="alignment">
+                        <i className="fa fa-envelope" onClick={clickNotificationIcon}>
+                            {notificationMenu ? <UserNotifications/> : null}
                         </i>
-                    </a>
-                    <a className="notification_icon">
-                        <div className="alignment">
-                            <i className="fa fa-envelope" onClick={this.clickNotificationIcon}>
-                                {this.state.menuNotification ? <UserNotifications/> : null}
-                            </i>
-                        </div>
-                    </a>
-                    <a href="#"><i className="fa fa-map"/></a>
-                </div>
+                    </div>
+                </a>
+                <a href="#"><i className="fa fa-map"/></a>
             </div>
         </div>
-    }
+    </div>
 }
 
 export default UserHeaderOptions
--- src/components/Header/UserHeader/UserNotifications/UserNotifications.js
+++ src/components/Header/UserHeader/UserNotifications/UserNotifications.js
@@ -1,22 +1,21 @@
-import React, {Component} from 'react';
+import React from 'react';
 import "./notifications.css"
 
-class UserNotifications extends Component {
-    render() {
-        return <div>
-            <div className="notification_number"><span>21</span></div>
-            <div className="message_box">
-                <ul>
-                    <li>
-                        <i className="message far fa-comment-dots"/>
-                        <span>David Chursa was already invited </span>
-                        <i className="close_mess fas fa-times"/>
-                    </li>
-                    <li/>
-                </ul>
-            </div>
+function UserNotifications() {
+    return <div>
+        <div className="notification_number"><span>21</span></div>
+        <div className="message_box">
+            <ul>
+                <li>
+                    <i className="message fa-comment-dots"/>
+                    <span>David Chursa was already invited </span>
+                    <i className="close_mess fa fa-times"/>
+                </li>
+                <li/>
+            </ul>
         </div>
-    }
+    </div>
+
 }
 
 export default UserNotifications
