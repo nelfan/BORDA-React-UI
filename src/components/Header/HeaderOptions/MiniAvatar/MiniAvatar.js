e8270497cdc3936a4026b7d212471144d289aa2d
jules <neroba.yulia@gmail.com>
2021-06-07 00:22:27 +0300

    Fixed style

--- src/components/Header/HeaderOptions/MiniAvatar/MiniAvatar.js
+++ src/components/Header/HeaderOptions/MiniAvatar/MiniAvatar.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import default_user from '../../../../assets/images/default-user.jpg';
 import Avatar from '../../../ProfilePage/Avatar/Avatar';
-import './miniAvatar.css';
 
 
-const MiniAvatar =() =>{
+const MiniAvatar = () => {
     const user = JSON.parse(sessionStorage.getItem('user'));
-    return <Avatar width = {user==null || user.avatar==null?"10%":"20%"} src={user==null || user.avatar==null?default_user:"data:image/jpeg;base64, "+user.avatar}></Avatar>
+    return <Avatar width={user == null || user.avatar == null ? "10%" : "20%"} src={user == null || user.avatar == null ? default_user : "data:image/jpeg;base64, " + user.avatar}></Avatar>
 }
 
 export default MiniAvatar;
