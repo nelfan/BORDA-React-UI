ffee5e145022bc11494af506d6af870e62359c29
nelfan <nelfan95@gmail.com>
2021-06-16 18:38:20 +0300

    Add checking boardId when redirect to /board

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -3,20 +3,25 @@ import "./board_page.css"
 import BoardList from "./BoardList/BoardList";
 import Header from '../Header/Header';
 import Proporties from './Proporties/Proporties';
+import {Redirect} from "react-router-dom";
 
 
 function Board(props) {
-    
-    const id = props.location.state.id;
-    return <div className="content">
-        <Header></Header>
-        <Proporties id = {id}></Proporties>
-        <div className="main_content">
-            <div className="active_borda_items">
-                <BoardList boardId={id}/>
+
+    if (props.location.state && props.location.state.id) {
+        const id = props.location.state.id;
+        return <div className="content">
+            <Header/>
+            <Proporties id={id}></Proporties>
+            <div className="main_content">
+                <div className="active_borda_items">
+                    <BoardList boardId={id}/>
+                </div>
             </div>
         </div>
-    </div>
+    } else {
+        return <Redirect to="/boards"  />
+    }
 }
 
 export default Board
