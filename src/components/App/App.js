b5d4d554a93b6f806bc566485106b3ad3e287b97
nelfan <nelfan95@gmail.com>
2021-06-16 18:15:59 +0300

    Add component PrivateRoute

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,23 +1,24 @@
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import Home from '../Home/Home';
 import Auth from '../Auth/Auth';
 import Boards from '../Boards/Boards';
 import Board from '../Board/Board';
 import PageNotFound from "../ErrorPage/PageNotFound";
+import PrivateRoute from "../ErrorPage/PrivateRoute";
 
 function App() {
 
-  return (
-    <Router>
-      <Switch>
-        <Route path='/' exact={true} component={Home} />
-        <Route path='/auth' exact={true} component={Auth} />
-        <Route path='/boards' exact={true} component={Boards} />
-        <Route path='/board' exact={true} component={Board} />
-        <Route component={PageNotFound}/>
-      </Switch>
-    </Router>
-  );
+    return (
+        <Router>
+            <Switch>
+                <Route path='/' exact={true} component={Home}/>
+                <Route path='/auth' exact={true} component={Auth}/>
+                <PrivateRoute path='/boards' exact={true} component={Boards}/>
+                <PrivateRoute path='/board' exact={true} component={Board}/>
+                <Route component={PageNotFound}/>
+            </Switch>
+        </Router>
+    );
 }
 
 export default App;
