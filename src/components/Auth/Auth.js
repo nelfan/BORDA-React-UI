2791370ef4acae2f37540b2f45b331fd8e877912
nelfan <nelfan95@gmail.com>
2021-06-18 18:33:31 +0300

    Change Auth component from class to function

--- src/components/Auth/Auth.js
+++ src/components/Auth/Auth.js
@@ -1,64 +1,61 @@
-import { Component } from 'react'
+import React, {useState} from 'react'
 import './auth.css';
 import Login from './Login/Login';
 import Register from './Register/Register';
-import { Redirect } from "react-router-dom";
-import React from 'react';
+import {Redirect} from "react-router-dom";
 
-class Auth extends Component {
+const Auth = () => {
+    const [showLogin, setShowLogin] = useState(false)
+    const [redirect, setRedirect] = useState(false)
 
-    onClickTabItem = () => {
-        this.showLogin = !this.showLogin
+    const onClickTabItem = () => {
+        setShowLogin(!showLogin)
     }
 
-    state = {
-        showLogin: false,
-        redirect: false
-    }
-
-  getUser = async () => {
-            const res = await fetch('http://localhost:9090/users/', {
-                method: 'GET',
-                headers: {
-                    'Authorization': 'Bearer ' + sessionStorage.getItem('jwtToken')
-                }
-            })
-            const data = await res.json();    
+    const getUser = async () => {
+        const res = await fetch('http://localhost:9090/users/', {
+            method: 'GET',
+            headers: {
+                'Authorization': 'Bearer ' + sessionStorage.getItem('jwtToken')
+            }
+        })
+        const data = await res.json();
         sessionStorage.setItem('user', JSON.stringify(data));
-   }
-
-    onSubmit = (token) => {
-        if (token) {
-            this.setState({ redirect: true })
-        }
-        this.getUser();
     }
 
-    render() {
-        if (this.state.redirect) {
-            return <Redirect to="/boards" />
+    const onSubmit = (token) => {
+        if (token) {
+            setRedirect(true)
         }
-        return (
-            <div className="main_container" >
-                <div className="align_form">
-                    <div className="form">
-                        <ul className="tab-group">
-                            <li className={!this.showLogin ? "tab active" : "tab"} onClick={this.onClickTabItem}><a href="#signup">Sign Up</a></li>
-                            <li className={this.showLogin ? "tab active" : "tab"} onClick={this.onClickTabItem}><a href="#login">Log In</a></li>
-                        </ul>
+        getUser();
+    }
 
-                        <div>
-                            {
-                                this.showLogin ? (
-                                    <Login onSubmitAuth={this.onSubmit} />) : (
-                                    <Register onSubmitAuth={this.onSubmit} />)
-                            }
-                        </div>
+    if (redirect) {
+        return <Redirect to="/boards"/>
+    }
+    return (
+        <div className="main_container">
+            <div className="align_form">
+                <div className="form">
+                    <ul className="tab-group">
+                        <li className={!showLogin ? "tab active" : "tab"} onClick={onClickTabItem}><a>Sign
+                            Up</a></li>
+                        <li className={showLogin ? "tab active" : "tab"} onClick={onClickTabItem}><a>Log
+                            In</a></li>
+                    </ul>
+
+                    <div>
+                        {
+                            showLogin ? (
+                                <Login onSubmitAuth={onSubmit}/>) : (
+                                <Register onSubmitAuth={onSubmit}/>)
+                        }
                     </div>
                 </div>
-            </div >
-        )
-    }
+            </div>
+        </div>
+    )
+
 }
 
 export default Auth
