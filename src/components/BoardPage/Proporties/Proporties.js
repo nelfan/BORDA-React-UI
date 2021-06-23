757212184d1c35b9743f326cec36a7836fd4ed08
jules <neroba.yulia@gmail.com>
2021-06-24 01:56:38 +0300

    Added display of board name

--- src/components/BoardPage/Proporties/Proporties.js
+++ src/components/BoardPage/Proporties/Proporties.js
@@ -7,13 +7,33 @@ const Proporties = (props) => {
 
     const id = props.id;
     const[seen, setSeen] = useState(false);
+    const[boardData, setBoardData] = useState([]);
     const showInvite = () => {
         setSeen(!seen);
       };
 
+      useEffect(() => {
+        const getBoardData = async () => {
+            const board = await fetchBoardData()
+            setBoardData(board);
+            console.log(board);
+        }
+        getBoardData()
+    }, [])
+
+    const fetchBoardData = async () => {
+        const res = await fetch('http://localhost:9090/boards/' + id, {
+            method: 'GET',
+            headers: {
+                'Authorization': 'Bearer ' + sessionStorage.getItem('jwtToken')
+            }
+        })
+        return await res.json()
+    }
 
     return <div className='proportiesLine'>
         <div className="boardOptions">
+            <div className="boardName">{boardData.name}</div>
         <i class="fa fa-user-plus" aria-hidden="true" onClick={showInvite}></i>
         </div>
         {(seen)&&<Invite id={id} close={showInvite}></Invite>}
