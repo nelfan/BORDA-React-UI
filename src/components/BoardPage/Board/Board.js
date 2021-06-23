7ff918d5509e4a2ce057f21715be3ea835e33533
nelfan <nelfan95@gmail.com>
2021-06-24 02:27:14 +0300

    add input checking for column title

--- src/components/BoardPage/Board/Board.js
+++ src/components/BoardPage/Board/Board.js
@@ -1,6 +1,7 @@
-import React, { useEffect, useState } from 'react';
+import React, {useEffect, useState} from 'react';
 import "./board.css"
 import BoardColumn from "./BoardColumn/BoardColumn";
+import Swal from "sweetalert2";
 
 function Board(props) {
 
@@ -12,11 +13,15 @@ function Board(props) {
         name: '',
         key: ''
     })
+    const [columnTitleError, setColumnTitleError] = useState('Title cannot be empty')
+    const ColumnTitleDirty = false
 
     const handleInput = (e) => {
+        columnTitleHandler(e.target.value)
         setCurrentBoardColumn({
             name: e.target.value
         })
+
     }
 
     useEffect(() => {
@@ -41,28 +46,59 @@ function Board(props) {
         return await res.json()
     }
 
+    const blurHandler = (e) => {
+        if (e.target.name === 'board_title') {
+            ColumnTitleDirty(true)
+        }
+    }
+
+    const columnTitleHandler = async (e) => {
+        const re = /^[\s~`!@#$%^&*()_+=[\]\\{}|;':",.\/<>?a-zA-Z0-9-]+$/;
+        if (e === '') {
+            setColumnTitleError('Title cannot be empty')
+        } else if (e.length > 25) {
+            setColumnTitleError('Title cannot be more than 25 characters')
+        } else if (e.trim().length === 0) {
+            setColumnTitleError('Title cannot contain only whitespace characters')
+        } else if (!re.test(String(e))) {
+            setColumnTitleError('Title can contain only english letters')
+        } else {
+            setColumnTitleError('')
+        }
+    }
+
     const addBoardColumn = async () => {
         const createBoardColumnData = {
             'name': currentBoardColumn.name
         }
 
-        const res = await fetch('http://localhost:9090/boards/' + boardId + '/columns', {
-            method: 'POST',
-            headers: {
-                'Content-type': 'application/json',
-                'Authorization': 'Bearer ' + sessionStorage.getItem('jwtToken')
-            },
-            body: JSON.stringify(createBoardColumnData),
-        })
+        if (columnTitleError !== '') {
+            Swal.fire({
+                title: 'Invalid column title',
+                text: columnTitleError,
+                icon: 'warning',
+                confirmButtonText: 'Try again',
+                confirmButtonColor: '#386DD8'
+            })
+        } else {
+            const res = await fetch('http://localhost:9090/boards/' + boardId + '/columns', {
+                method: 'POST',
+                headers: {
+                    'Content-type': 'application/json',
+                    'Authorization': 'Bearer ' + sessionStorage.getItem('jwtToken')
+                },
+                body: JSON.stringify(createBoardColumnData),
+            })
 
-        const data = await res.json()
+            const data = await res.json()
 
-        setBoardColumns([...boardColumns, data])
+            setBoardColumns([...boardColumns, data])
 
-        setCurrentBoardColumn({
-            name: '',
-            key: ''
-        })
+            setCurrentBoardColumn({
+                name: '',
+                key: ''
+            })
+        }
     }
 
     const newBoardColumn = () => {
@@ -108,10 +144,12 @@ function Board(props) {
     }
 
     const list = boardColumns.map(boardColumn => {
-        return <li key={boardColumn.id}><BoardColumn boardId={boardId} data={boardColumn} 
-            updateBoardColumn={updateBoardColumn} refreshBoardColumns={refreshBoardColumns} 
-            deleteBoardColumn={deleteBoardColumn} toggleUpdateBoardColumnTickets={toggleUpdateBoardColumnTickets}
-            updateBoardColumnTickets={updateBoardColumnTickets} /></li>
+        return <li key={boardColumn.id}><BoardColumn boardId={boardId} data={boardColumn}
+                                                     updateBoardColumn={updateBoardColumn}
+                                                     refreshBoardColumns={refreshBoardColumns}
+                                                     deleteBoardColumn={deleteBoardColumn}
+                                                     toggleUpdateBoardColumnTickets={toggleUpdateBoardColumnTickets}
+                                                     updateBoardColumnTickets={updateBoardColumnTickets}/></li>
     })
 
     return <ul className="default_main" id="defaultMain">
@@ -126,7 +164,7 @@ function Board(props) {
                                 <span className="cancel_board" onClick={cancelAddNewBoardColumn}> Cancel </span>
                             </div> :
                             <div className="add_board" onClick={newBoardColumn}>
-                                <span> Add new Column <i className="fa fa-plus" /> </span>
+                                <span> Add new Column <i className="fa fa-plus"/> </span>
                             </div>
                         }
 
@@ -134,9 +172,11 @@ function Board(props) {
                             <div className="textfield_of_newboard">
                                 <div className="align_of_board_content">
                                     <div className="board_title">
-                                        <input id="board_title" onChange={handleInput} type="text"
-                                            placeholder="Enter a column title" autoComplete="off"
-                                            value={currentBoardColumn.name} required />
+                                        <input id="board_title" onBlur={e => {
+                                            blurHandler(e)
+                                        }} onChange={handleInput} type="text"
+                                               placeholder="Enter a column title" autoComplete="off"
+                                               value={currentBoardColumn.name} required/>
                                     </div>
                                 </div>
                             </div> : null}
